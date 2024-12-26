7f54fcfaf1dac21e89c9654aad085caa211d0b91
Dan <55022836+gsmj@users.noreply.github.com>
2024-12-26 09:16:19 +0300

    Update modes.py

--- pymapandreas/modes.py
+++ pymapandreas/modes.py
@@ -1,7 +1,6 @@
 class MapAndreasMode:
-    MAP_ANDREAS_MODE_NONE: int = 0
-    MAP_ANDREAS_MODE_MINIMAL: int = 1
-    MAP_ANDREAS_MODE_MEDIUM: int = 2
-    MAP_ANDREAS_MODE_FULL: int = 3
-    MAP_ANDREAS_MODE_NOBUFFER: int = 4
-    
+    MODE_NONE: int = 0
+    MODE_MINIMAL: int = 1
+    MODE_MEDIUM: int = 2
+    MODE_FULL: int = 3
+    MODE_NOBUFFER: int = 4
