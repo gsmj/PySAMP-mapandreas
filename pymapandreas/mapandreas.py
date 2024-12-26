404fbe5dee28db3e167d84f55cebde5aeee0bb7c
Dan <55022836+gsmj@users.noreply.github.com>
2024-12-26 09:13:44 +0300

    Update mapandreas.py

--- pymapandreas/mapandreas.py
+++ pymapandreas/mapandreas.py
@@ -13,22 +13,17 @@ class MapAndreas():
         self.name = name
         map_andreas_init(mode, name)
 
-
     def find_z_for_2d_coord(self, x: float, y: float):
         return map_andreas_find_z_for_2d_coord(x, y)
-
 
     def map_andreas_find_average_z(self, x: float, y: float):
         return map_andreas_find_average_z(x, y)
 
-
     def unload(self):
         return map_andreas_unload()
 
-
     def map_andreas_set_z_for_2d_coord(self, x: float, y: float, z: float):
         return map_andreas_set_z_for_2d_coord(x, y, z)
-    
     
     def map_andreas_save(self):
         return map_andreas_save_current_map(self.name)
